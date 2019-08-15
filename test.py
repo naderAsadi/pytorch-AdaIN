78d68ca49ebea70ed3709d31fda01635eb428288
nader <nader.asadi76@gmail.com>
2019-08-15 11:34:03 +0430

    Initial

--- test.py
+++ test.py
@@ -56,13 +56,13 @@ parser.add_argument('--style', type=str,
 parser.add_argument('--style_dir', type=str,
                     help='Directory path to a batch of style images')
 parser.add_argument('--vgg', type=str, default='models/vgg_normalised.pth')
-parser.add_argument('--decoder', type=str, default='models/decoder_iter_280000_photo2all_75.pth.tar')
+parser.add_argument('--decoder', type=str, default='models/photo2art_decoder_iter_130000.pth.tar')
 
 # Additional options
-parser.add_argument('--content_size', type=int, default=75,
+parser.add_argument('--content_size', type=int, default=256,
                     help='New (minimum) size for the content image, \
                     keeping the original size if set to 0')
-parser.add_argument('--style_size', type=int, default=75,
+parser.add_argument('--style_size', type=int, default=256,
                     help='New (minimum) size for the style image, \
                     keeping the original size if set to 0')
 parser.add_argument('--crop', action='store_true',
